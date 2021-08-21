cd7c09c7dad099d483b20bae0edc11d9519f8953
kbgro <daniendegwa@gmail.com>
2021-08-21 22:43:22 +0300

    Single Page

---
+++ src/main/java/com/github/kbgro/books/SinglePage.java
@@ -0,0 +1,56 @@
+package com.github.kbgro.books;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SinglePage {
+    private final WebDriver driver;
+
+    @FindBy(css = "ul.pager li.previous a")
+    WebElement previous;
+
+    @FindBy(css = "ul.pager li.next a")
+    WebElement next;
+
+    @FindBy(css = "ul.pager li.current")
+    WebElement current;
+
+    @FindBy(css = "ol.row li .image_container>a")
+    List<WebElement> productLinks;
+
+    public SinglePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean hasNextPage() {
+        return nextPrevElement("ul.pager li.next a") != null;
+    }
+
+    public boolean hasPrevPage() {
+        return nextPrevElement("ul.pager li.previous a") != null;
+    }
+
+    public List<String> getProductLinks() {
+        List<String> links = new ArrayList<>(20);
+        for (final WebElement link : productLinks)
+            links.add(link.getAttribute("href").strip());
+        return links;
+    }
+
+    private WebElement nextPrevElement(String css) {
+        WebElement nextPrev = null;
+        try {
+            nextPrev = driver.findElement(By.cssSelector(css));
+        } catch (NoSuchElementException ignored) {
+        }
+        return nextPrev;
+    }
+}
---
+++ src/test/java/com/github/kbgro/books/SinglePageTest.java
@@ -0,0 +1,62 @@
+package com.github.kbgro.books;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+class SinglePageTest {
+    private WebDriver driver;
+    private SinglePage singlePage;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://books.toscrape.com");
+
+        singlePage = new SinglePage(driver);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testNextPage() {
+        driver.get("https://books.toscrape.com/catalogue/page-50.html");
+        Assertions.assertFalse(singlePage.hasNextPage());
+        Assertions.assertThrows(NoSuchElementException.class, () -> singlePage.next.getText());
+    }
+
+    @Test
+    public void testPrevPage() {
+        Assertions.assertFalse(singlePage.hasPrevPage());
+        Assertions.assertThrows(NoSuchElementException.class, () -> singlePage.previous.getText()
+        );
+    }
+
+    @Test
+    public void testNextPrevPage() {
+        driver.get("https://books.toscrape.com/catalogue/page-2.html");
+        Assertions.assertTrue(singlePage.hasNextPage());
+        Assertions.assertTrue(singlePage.hasPrevPage());
+        Assertions.assertEquals(singlePage.current.getText().strip(), "Page 2 of 50");
+    }
+
+    @Test
+    public void testProductLinks() {
+        Assertions.assertEquals(singlePage.getProductLinks().size(), 20);
+        List<String> links = singlePage.getProductLinks();
+        Assertions.assertTrue(links.get(0).contains("https://books.toscrape.com/catalogue/"));
+    }
+}
