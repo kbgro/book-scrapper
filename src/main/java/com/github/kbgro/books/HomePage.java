51a4fd0d1e25e7377d64c797c142b89d5e225c9e
kbgro <daniendegwa@gmail.com>
2021-08-21 21:10:26 +0300

    Home page

---
+++ src/main/java/com/github/kbgro/books/HomePage.java
@@ -0,0 +1,27 @@
+package com.github.kbgro.books;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HomePage {
+    @FindBy(css = "div.side_categories>ul a[href^=catalogue]")
+    public List<WebElement> categories;
+
+    public HomePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public Map<String, WebElement> titledCategories() {
+        Map<String, WebElement> cat = new HashMap<>();
+        for (final WebElement link : categories) {
+            cat.put(link.getText(), link);
+        }
+        return cat;
+    }
+}
---
+++ src/test/java/com/github/kbgro/books/HomePageTest.java
@@ -0,0 +1,40 @@
+package com.github.kbgro.books;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomePageTest {
+    private WebDriver driver;
+    private HomePage homePage;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://books.toscrape.com");
+
+        homePage = new HomePage(driver);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testCategories() {
+        var cat = homePage.titledCategories();
+        Assertions.assertEquals(cat.keySet().size(), 51);
+    }
+
+}
