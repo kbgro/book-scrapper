f46c26058ff79b25ba81e508357c8f0c5e7f11fa
kbgro <daniendegwa@gmail.com>
2021-08-23 19:10:48 +0300

    set category links

--- src/main/java/com/github/kbgro/books/pages/HomePage.java
+++ src/main/java/com/github/kbgro/books/pages/HomePage.java
@@ -12,16 +12,30 @@ import java.util.Map;
 public class HomePage {
     @FindBy(css = "div.side_categories>ul a[href^=catalogue]")
     public List<WebElement> categories;
+    public Map<String, String> categoryLinks = new HashMap<>();
 
     public HomePage(WebDriver driver) {
         PageFactory.initElements(driver, this);
+        setCategoryLinks();
+    }
+
+    public Map<String, String> getCategoryLinks() {
+        return categoryLinks;
     }
 
     public Map<String, WebElement> titledCategories() {
         Map<String, WebElement> cat = new HashMap<>();
         for (final WebElement link : categories) {
             cat.put(link.getText(), link);
+            categoryLinks.put(link.getText(), link.getAttribute("href"));
         }
         return cat;
+    }
+
+    private void setCategoryLinks() {
+        Map<String, WebElement> cat = new HashMap<>();
+        for (final WebElement link : categories) {
+            categoryLinks.put(link.getText(), link.getAttribute("href"));
+        }
     }
 }
